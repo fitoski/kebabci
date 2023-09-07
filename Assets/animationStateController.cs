d9fc2db244bcde2de2551285b2f41d89a14e4da1
Alper Arslan <alperarslan@metasapienstudio.com>
2023-09-07 16:45:17 +0300

    movement, animation fix

--- Assets/animationStateController.cs
+++ Assets/animationStateController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AnimationStateController : MonoBehaviour
 {
     private Animator animator; // Changed access modifier to private
+    [SerializeField] private Transform cameraPivot;
+    [SerializeField] private Transform cameraFollowPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -13,10 +15,21 @@ public class AnimationStateController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, z).normalized;
+
+        if (movement.magnitude > 0)
+        {
+            transform.position += movement * 5f * Time.deltaTime;
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+
         // If the player presses the 'w' key
-        if (Input.GetKey("w"))
+        if (movement.magnitude > 0)
         {
             // Set the "isWalking" boolean parameter in the animator to true
             animator.SetBool("isWalking", true);
@@ -26,5 +39,7 @@ public class AnimationStateController : MonoBehaviour
             // If the 'w' key is not being pressed, set the "isWalking" parameter to false
             animator.SetBool("isWalking", false);
         }
+
+        cameraPivot.transform.position = cameraFollowPoint.transform.position;
     }
 }
